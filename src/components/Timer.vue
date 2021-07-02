744611ac53f860e60d33e19e01fa46053fa68b06
Hendridev <hendridevlp@gmail.com>
2021-07-02 17:47:14 +0700

    pushing song

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,24 +1,35 @@
 <template>
   <div>
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="/med/logo.svg">
+    <h1>Pomotime</h1>
+    <label for="cx">Sound off</label>
+      <input v-model="enableSong" type="checkbox" name="cx" id="cx" placeholder="Sound off">
+    <div>
+      <select @change="songChange">
+        <option v-for="song in songlist" :key="song.name" :value="song.value">{{song.name}}</option>
+      </select>
+    </div>
     <div class="select-timer">
       <button @click="setTime(1500)">25:00</button>
       <button @click="setTime(3600)">60:00</button>
       <button @click="setTime(5400)">90:00</button>
       <button @click="setTime(7200)">120:00</button>
     </div>
-    <h1>Pomotime</h1>
-    <h2>{{Timeleft}}</h2>
+    <div>
+      <p>current song : {{currentSong}}</p>
+    </div>
+    <div class="timewrapper">
+      <div>
+        <h2>{{Timeleft}}</h2>
+      </div>
+    </div>
     <div v-if="play">
       <button @click="startInterval">{{status}}</button>
     </div>
     <div v-else>
       <button @click="stopInterval">{{status}}</button>
     </div>
-     <audio ref="audio" loop id="audio">
-        <source src="../assets/firstdate.mp3" type="audio/ogg">
-           Your browser does not support the audio element.
-    </audio> 
+      <audio ref="audio" loop id="audio" :src="audioSrc"></audio>
   </div>
 </template>
 
@@ -27,11 +38,26 @@ export default {
   name: 'Timer',
   data(){
     return{
+      enableSong: false,
       status : 'Start',
       limitTime: 1500,
       passTime: 0,
       startTimer: null,
-      play: true
+      play: true,
+      currentSong: 'first date',
+      audioSrc: '/med/firstdate.mp3',
+      songlist: [
+        {name: 'First date', value: '/med/firstdate.mp3'},
+        {name: 'Ylang ylang', value: '/med/ylang.mp3'},
+        {name: 'Relax', value: '/med/relax.mp3'},
+        {name: 'Spirited away', value: '/med/spiritedaway.mp3'},
+        {name: 'Shelter', value: '/med/shelter.mp3'},
+        {name: 'Nandemonaiya', value: '/med/nandemonay.mp3'},
+        {name: 'New World', value: '/med/wholenewworld.mp3'},
+        {name: 'Summertime', value: '/med/summertime.mp3'},
+        {name: 'Happier', value: '/med/happier.mp3'},
+        {name: 'Halu', value: '/med/halu.mp3'},
+      ]
     }
   },
   computed:{
@@ -47,12 +73,21 @@ export default {
     }
   },
   methods: {
+    songChange(e){
+      this.audioSrc = e.target.value; 
+      this.currentSong = this.songlist[e.target.selectedIndex].name;
+    },
     startInterval(){
       this.play = false;
       this.status = 'Stop';
         this.startTimer = setInterval(() => {
           this.passTime ++;
-          document.querySelector('#audio').play();
+          if(this.enableSong == false){
+            document.querySelector('#audio').play();
+          }
+          if(this.enableSong == true){
+             document.querySelector('#audio').pause();
+          }
           if(this.passTime == this.limitTime){
             clearInterval(this.startTimer);
               document.querySelector('#audio').pause();
@@ -91,17 +126,43 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  *{
+    font-family: 'Poppins', sans-serif;
+  }
+  img{
+    width: 80px;
+  }
   button{
     padding: 4px 8px;
-    font-size: 1.2em;
+    font-size: 1em;
     color: white;
     background-color: rgb(50, 219, 135);
     border: 1px solid rgb(86, 196, 86);
+    border-radius: 4px;
+  }
+  select{
+    padding: 4px 6px;
+    font-size: 0.9em;
+    color: white;
+    background-color: rgb(50, 219, 135);
+    border: 1px solid rgb(86, 196, 86);
+    margin: 8px 0 12px 0;
+    border-radius: 4px;
   }
   .select-timer button{
     margin: 0 4px;
   }
   button:hover{
     background-color: rgb(71, 194, 71);
+  }
+  .timewrapper{
+    padding: 4px;
+    display: flex;
+    justify-content: center;
+  }
+  .timewrapper div{
+    padding: 4px 24px;
+    border: 1px solid rgb(50, 219, 135);
+    margin-bottom: 4px;
   }
 </style>
